4a4bbb9371eca475d2343fba28605e53120ee064
patryk-chwastek <patrykchp@gmail.com>
2022-01-12 09:50:40 +0100

    drag and drop employTasks

--- human-resources-manager-client/src/Components/UserTasks.js
+++ human-resources-manager-client/src/Components/UserTasks.js
@@ -48,17 +48,17 @@ const onDragEnd = (result, columns, setColumns) => {
 
 const UserTasks = () => {
     const [columns, setColumns] = useState({
-        ["requested"]: {
+        ['requested']: {
             columnId: "requested",
             name: "Requested",
             items: []
         },
-        ["todo"]: {
+        ['todo']: {
             columnId: "todo",
             name: "To do",
             items: []
         },
-        ["inprogress"]: {
+        ['inprogress']: {
             columnId: "inprogress",
             name: "In Progress",
             items: []
@@ -66,111 +66,118 @@ const UserTasks = () => {
     });
 
     useEffect(() => {
-        getTasks(1, 10, 12, "Reqested").then((data) => {
-            setColumns({
-                ...columns, ['reqested']:
-                    items: data.items
+        getTasks(1, 10, 12, 'Requested').then((data) => {
+            adddItemsToColumn(columns.requested, data.items)
+            tasksColumns[2].items = data.items;
+            console.log(columns);
+            console.log("--------------------------------");
+            console.log(data);
+        });;
+    }, []);
+
+    const adddItemsToColumn = (column, items) => {
+        setColumns({
+            ...columns, [column.columnId]: {
+                ...column,
+                items: items
             }
-            })
-        tasksColumns[2].items = data.items;
-        console.log(columns);
-    });;
-}, []);
-
-const getTasks = async (page, size, employeeid, status) => {
-    const requestOptions = {
-        method: 'Get',
-        headers: { 'Content-Type': 'application/json' }
-    };
-    return await fetch(APIURL +
-        `tasks?page=${page}&size=${size}&employeeid=${employeeid}&status=${status}`,
-        requestOptions
-    ).then((response) => {
-        if (response.ok)
-            return response.json();
-        else
-            return Promise.reject();
-    })
-        .then(data => {
-            return data
         })
-}
+    }
 
-return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-        <DragDropContext
-            onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-        >
-            {Object.entries(columns).map(([columnId, column], index) => {
-                return (
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center"
-                        }}
-                        key={columnId}
-                    >
-                        <h2>{column.name}</h2>
-                        <div style={{ margin: 8 }}>
-                            <Droppable droppableId={columnId} key={columnId}>
-                                {(provided, snapshot) => {
-                                    return (
-                                        <div
-                                            {...provided.droppableProps}
-                                            ref={provided.innerRef}
-                                            style={{
-                                                background: snapshot.isDraggingOver
-                                                    ? "lightblue"
-                                                    : "lightgrey",
-                                                padding: 4,
-                                                width: 250,
-                                                minHeight: 500
-                                            }}
-                                        >
-                                            {column.items.map((item, index) => {
-                                                return (
-                                                    <Draggable
-                                                        key={item.id}
-                                                        draggableId={item.id}
-                                                        index={index}
-                                                    >
-                                                        {(provided, snapshot) => {
-                                                            return (
-                                                                <div
-                                                                    ref={provided.innerRef}
-                                                                    {...provided.draggableProps}
-                                                                    {...provided.dragHandleProps}
-                                                                    style={{
-                                                                        userSelect: "none",
-                                                                        padding: 16,
-                                                                        margin: "0 0 8px 0",
-                                                                        minHeight: "50px",
-                                                                        backgroundColor: snapshot.isDragging
-                                                                            ? "#263B4A"
-                                                                            : "#456C86",
-                                                                        color: "white",
-                                                                        ...provided.draggableProps.style
-                                                                    }}
-                                                                >
-                                                                    {item.name}
-                                                                </div>
-                                                            );
-                                                        }}
-                                                    </Draggable>
-                                                );
-                                            })}
-                                            {provided.placeholder}
-                                        </div>
-                                    );
-                                }}
-                            </Droppable>
+    const getTasks = async (page, size, employeeid, status) => {
+        const requestOptions = {
+            method: 'Get',
+            headers: { 'Content-Type': 'application/json' }
+        };
+        return await fetch(APIURL +
+            `tasks?page=${page}&size=${size}&employeeid=${employeeid}&status=${status}`,
+            requestOptions
+        ).then((response) => {
+            if (response.ok)
+                return response.json();
+            else
+                return Promise.reject();
+        })
+            .then(data => {
+                return data
+            })
+    }
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+            <DragDropContext
+                onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+            >
+                {Object.entries(columns).map(([columnId, column], index) => {
+                    return (
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center"
+                            }}
+                            key={columnId}
+                        >
+                            <h2>{column.name}</h2>
+                            <div style={{ margin: 8 }}>
+                                <Droppable droppableId={columnId} key={columnId}>
+                                    {(provided, snapshot) => {
+                                        return (
+                                            <div
+                                                {...provided.droppableProps}
+                                                ref={provided.innerRef}
+                                                style={{
+                                                    background: snapshot.isDraggingOver
+                                                        ? "lightblue"
+                                                        : "lightgrey",
+                                                    padding: 4,
+                                                    width: 250,
+                                                    minHeight: 500
+                                                }}
+                                            >
+                                                {column.items.map((item, index) => {
+                                                    return (
+                                                        <Draggable
+                                                            key={item.id}
+                                                            draggableId={String(item.id)}
+                                                            index={index}
+                                                        >
+                                                            {(provided, snapshot) => {
+                                                                return (
+                                                                    <div
+                                                                        ref={provided.innerRef}
+                                                                        {...provided.draggableProps}
+                                                                        {...provided.dragHandleProps}
+                                                                        style={{
+                                                                            userSelect: "none",
+                                                                            padding: 16,
+                                                                            margin: "0 0 8px 0",
+                                                                            minHeight: "50px",
+                                                                            backgroundColor: snapshot.isDragging
+                                                                                ? "#263B4A"
+                                                                                : "#456C86",
+                                                                            color: "white",
+                                                                            ...provided.draggableProps.style
+                                                                        }}
+                                                                    >
+                                                                        {item.name}
+                                                                    </div>
+                                                                );
+                                                            }}
+                                                        </Draggable>
+                                                    );
+                                                })}
+                                                {provided.placeholder}
+                                            </div>
+                                        );
+                                    }}
+                                </Droppable>
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
-        </DragDropContext>
-    </div>
-);
+                    );
+                })}
+            </DragDropContext>
+        </div>
+    );
 }
 export default UserTasks;
